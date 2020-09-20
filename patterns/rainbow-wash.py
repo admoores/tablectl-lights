ed3a0f6dc504d1e4993f46aa7fcba8836da1cada
admoores <moores.alexd@gmail.com>
2020-09-19 22:17:45 -0400

    tester

--- patterns/rainbow-wash.py
+++ patterns/rainbow-wash.py
@@ -8,4 +8,8 @@ while True:
   for i in range(36):
     pixels.fill((0, 0, 0))
     pixels[i] = (255, 0, 255);
-  time.sleep(.1)
+    pixels[(i + 1) % 36] = (255, 0, 255);
+    pixels[(i + 2) % 36] = (255, 0, 255);
+    pixels[(i + 3) % 36] = (255, 0, 255);
+    pixels[(i + 4) % 36] = (255, 0, 255);
+  time.sleep(.25)
