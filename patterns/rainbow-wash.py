baf7c27bd65df207354458a4a7bfe36fdaa71f29
admoores <moores.alexd@gmail.com>
2020-09-19 22:15:49 -0400

    tester

--- patterns/rainbow-wash.py
+++ patterns/rainbow-wash.py
@@ -7,5 +7,5 @@ pixels = neopixel.NeoPixel(board.D18, 36)
 while True:
   for i in range(36):
     pixels.fill((0, 0, 0))
-    pixels[i:i + 5] = (255, 0, 255);
+    pixels[i] = (255, 0, 255);
   time.sleep(.1)
