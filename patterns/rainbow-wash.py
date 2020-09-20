9ce35a6a60d1c9b5108d21fa8cae11960b3bdc2e
admoores <moores.alexd@gmail.com>
2020-09-19 22:05:49 -0400

    another test

--- patterns/rainbow-wash.py
+++ patterns/rainbow-wash.py
@@ -2,8 +2,14 @@ import board
 import neopixel
 
 pixels = neopixel.NeoPixel(board.D18, 36)
+offset = 0
 
-for r in range(0, 255, 5):
-  for g in range(0, 255, 5):
-    for b in range(0, 255, 5):
-       pixels.fill((r, g, b))
+while true:
+  for i in range(36):
+    pixels[i] = (
+      ((i + offset) % 3) == 0 ? 255 : 0,
+      ((i + offset) % 3) == 1 ? 255 : 0,
+      ((i + offset) % 3) == 2 ? 255 : 0,
+    )
+  time.sleep(500)
+  offset++
